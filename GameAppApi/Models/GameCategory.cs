632d53e7991e46a7e5d80aa67f6914aabf0a131e
MD. JOBAYER ARAFAT <mdjobayerarafat@gmail.com>
2024-11-11 01:56:51 +0600

    Add project files.

---
+++ GameAppApi/Models/GameCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace GameAppApi.Models
+{
+    public class GameCategory
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Thumbnail { get; set; }
+
+        public List<Game> Games { get; set; } = new List<Game> ();
+    }
+}
