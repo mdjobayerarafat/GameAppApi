444ccef52190cd776023d7932b32aa8ddfb85771
MD. JOBAYER ARAFAT <mdjobayerarafat@gmail.com>
2024-11-11 02:02:47 +0600

    game

--- GameAppApi/Models/Game.cs
+++ GameAppApi/Models/Game.cs
@@ -1,6 +1,17 @@
-﻿namespace GameAppApi.Models
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace GameAppApi.Models
 {
     public class Game
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Thumbnail { get; set; }
+        public required string DownloadLink { get; set; }
+
+        public int GameCategoryId { get; set; }
+        public required GameCategory GameCategory { get; set; }
+
     }
 }
